710f43aa48064de5a995cfc37c5752a21a8721ab
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-02 16:32:38 +0100

    mock requests

--- backend/tests/test_get_suggestions.py
+++ backend/tests/test_get_suggestions.py
@@ -1,11 +1,13 @@
 from fastapi.testclient import TestClient
 from backend.main import app
+from requests_mock import ANY
 
 client = TestClient(app)
 
 
-# TODO: mock requests.get
-def test_get_suggestions():
+def test_get_suggestions(requests_mock):
+    requests_mock.get(ANY, json=["wer wie was", ["sugestion1", "sugestion2"]])
     response = client.get("/mountainbike")
-    assert response.status_code == 200
 
+    assert response.status_code == 200
+    assert requests_mock.call_count == 3  # 3 requests are made for q = ['wer', 'wie', 'was']
