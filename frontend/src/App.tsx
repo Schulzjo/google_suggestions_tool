03ad38cb826a844cebe48898859b7fe6c8668e38
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-06 15:00:03 +0100

    refactor form using material ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
+import {
+    Button,
+    FormControl,
+    TextField,
+} from "@mui/material"
 
 interface ApiResponse {
     message: { [key: string]: string[] };
 }
 
+interface FormValues {
+    keyword?: string;
+
+}
 
 function App() {
 
     const [data, setData] = useState<ApiResponse>();
+    const [formValues, setFormValues] = useState<FormValues>({});
+    const handleTextFieldChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const {name, value} = event.target;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
 
     function fetchData(keyword: string) {
         fetch("http://localhost:8000/" + keyword)
@@ -16,29 +35,46 @@ function App() {
             .then(d => setData({...d}))
     }
 
-    function handleSubmit(event: any) {
-        event.preventDefault();
-        fetchData(event.target.keyword.value);
+    function handleSubmit() {
+        if (formValues.keyword) {
+            fetchData(formValues.keyword);
+        } else {
+            console.log("no keyword");
+        }
+
     }
 
     return (
         <div className="App">
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="keyword" placeholder="keyword"/>
-                <button>Submit</button>
-                <div className={"list centered"}>
-                    <ul>
-                        {data && Object.keys(data.message).map((key) => (
-                            <li>{key}
-                                <ul>
-                                    {data.message[key].map((value) => (<li>{value}</li>))}
-                                </ul>
-                            </li>
-                        ))
-                        }</ul>
-                </div>
-            </form>
-
+            <FormControl sx={{display: "flex", flexDirection: "row", padding: "10px"}}>
+                <TextField
+                    autoFocus
+                    fullWidth
+                    name="keyword"
+                    label="Suchbegriff"
+                    variant="outlined"
+                    onChange={handleTextFieldChange}
+                />
+                <Button
+                    sx={{ml: 1}}
+                    type="submit"
+                    variant="contained"
+                    onClick={handleSubmit}
+                >
+                    Suchen
+                </Button>
+            </FormControl>
+            <div className={"list centered"}>
+                <ul>
+                    {data && Object.keys(data.message).map((key) => (
+                        <li>{key}
+                            <ul>
+                                {data.message[key].map((value) => (<li>{value}</li>))}
+                            </ul>
+                        </li>
+                    ))
+                    }</ul>
+            </div>
         </div>
     );
 }
