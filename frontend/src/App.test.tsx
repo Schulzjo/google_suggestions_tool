01c4b2a7bdba633f22436ae9efe2e32453715106
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-09 12:20:10 +0100

    implement initial routing

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import {MemoryRouter} from "react-router-dom"
 
 test('renders App', () => {
-  render(<App />);
+    render(
+        <MemoryRouter initialEntries={["/"]}>
+            <App/>
+        </MemoryRouter>);
 });
