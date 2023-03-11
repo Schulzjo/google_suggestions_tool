8b6c3539c6c75e15c82c9d3cc1b411274cf310a9
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-11 19:03:00 +0100

    add searchfield to navbar when app is on result route

--- frontend/src/layout/Appbar/Appbar.tsx
+++ frontend/src/layout/Appbar/Appbar.tsx
@@ -1,21 +1,28 @@
 import {AppBar as AppBarMUI, Box, Toolbar, Typography} from "@mui/material"
+import {useMatch, useNavigate} from "react-router-dom";
+import SearchField from "../../components/SearchField";
+import React from "react";
 
 interface AppbarProps {
 
 }
 
 const Appbar: React.FC<AppbarProps> = () => {
+    const matchResultRoute = useMatch('/result/:param');
+    console.log(matchResultRoute?.params.param);
 
-
-  return (
-    <AppBarMUI position="fixed">
-      <Toolbar>
-        <Box>
-         <Typography>Google Suggestion Tool</Typography>
-        </Box>
-      </Toolbar>
-    </AppBarMUI>
-  )
+    return (
+        <AppBarMUI position="fixed">
+            <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+                <Box>
+                    <Typography>Google Suggestion Tool</Typography>
+                </Box>
+                <Box style={{ width: "50%", margin: "0 auto" }}>
+                    {matchResultRoute && <SearchField/>}
+                </Box>
+            </Toolbar>
+        </AppBarMUI>
+    )
 }
 
 export default Appbar
