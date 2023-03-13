2f20ef4ae8a985809641673413a4d5c4c9fe63e0
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-13 13:40:46 +0100

    add bordered box arround searchfield

--- frontend/src/pages/SearchFieldPage.tsx
+++ frontend/src/pages/SearchFieldPage.tsx
@@ -1,14 +1,19 @@
 import React, {useState} from "react";
 import SearchField from "../components/SearchField";
+import {Box, Grid} from "@mui/material";
 
 
 const SearchFieldPage = () => {
 
 
     return (
-        <div>
-            <SearchField/>
-        </div>
+
+        <Grid style={{height: '70vh', alignItems:"center", justifyContent:"center", display: "flex"}}>
+            <Box border={1} borderRadius={3} p={10} boxShadow={3} width={"50%"} >
+                <SearchField/>
+            </Box>
+        </Grid>
+
     );
 }
 
