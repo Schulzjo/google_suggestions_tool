7e30ade946f4e88fe22ddc143362d0c5d7c8bf1c
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-14 17:05:08 +0100

    add blurry effekt to ard like elements

--- frontend/src/components/ResultCards.tsx
+++ frontend/src/components/ResultCards.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Card, CardContent, Grid, Typography} from "@mui/material";
 
 
-
 interface ResultCardsProps {
     result: { [key: string]: string[] };
 }
@@ -10,22 +9,29 @@ interface ResultCardsProps {
 
 const ResultCards: React.FC<ResultCardsProps> = ({result}) => {
     return (
-            <Grid container spacing={12} sx={{padding: "10px"}}>
-                {result && Object.keys(result).map((key) => (
-                    <Grid key={key} item xs={12} md={6} lg={4}>
-                        <Card variant="outlined" sx={{height: "250px", overflow: 'auto'}}>
-                            <CardContent sx={{justify: "flex-start"}}>
-                                <Typography variant="h5" component="div" gutterBottom>
-                                    {key}
-                                </Typography>
-                                    {result[key].map((value) => (
-                                    <Typography key={value + key} variant="body1">{value}<br/></Typography>))}
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))
-                }
-            </Grid>
+        <Grid container spacing={12} sx={{padding: "10px"}}>
+            {result && Object.keys(result).map((key) => (
+                <Grid key={key} item xs={12} md={6} lg={4}>
+                    <Card variant="outlined" sx={{
+                        height: "250px",
+                        border: 1,
+                        borderRadius: 3,
+                        boxShadow: 3,
+                        overflow: 'auto',
+                        backgroundColor: "transparent"
+                    }} className={"blurry"}>
+                        <CardContent sx={{justify: "flex-start"}}>
+                            <Typography variant="h5" component="div" gutterBottom>
+                                {key}
+                            </Typography>
+                            {result[key].map((value) => (
+                                <Typography key={value + key} variant="body1">{value}<br/></Typography>))}
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))
+            }
+        </Grid>
     );
 };
 
--- frontend/src/pages/SearchFieldPage.tsx
+++ frontend/src/pages/SearchFieldPage.tsx
@@ -8,8 +8,8 @@ const SearchFieldPage = () => {
 
     return (
 
-        <Grid style={{height: '70vh', alignItems:"center", justifyContent:"center", display: "flex"}}>
-            <Box border={1} borderRadius={3} p={10} boxShadow={3} width={"50%"} >
+        <Grid style={{height: '70vh', alignItems: "center", justifyContent: "center", display: "flex"}}>
+            <Box border={1} borderRadius={3} p={10} boxShadow={3} width={"50%"} className={"blurry"}>
                 <SearchField/>
             </Box>
         </Grid>
