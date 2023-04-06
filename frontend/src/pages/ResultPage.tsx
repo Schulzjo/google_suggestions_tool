13777993cc52e7d32be055c563e1ffbc3692abc4
schulzjo <schulzeeverding.johannes@gmail.com>
2023-04-06 10:20:03 +0200

    initial implementation of Loading state

--- frontend/src/pages/ResultPage.tsx
+++ frontend/src/pages/ResultPage.tsx
@@ -9,26 +9,31 @@ interface ApiResponse {
 const ResultPage = () => {
     const {param} = useParams();
     const [data, setData] = useState<ApiResponse>();
+    let [isLoading, setIsLoading] = useState<boolean>(true)
 
     function fetchData(keyword: string) {
         fetch("http://localhost:8000/" + keyword)
             .then(response => response.json())
             .then(d => setData({...d}))
+            .then(() => setIsLoading(false))
     }
 
     useEffect(() => {
-            if (param) {
-                fetchData(param);
-            } else {
-                console.log("no keyword");
-            }
-        }, [param]);
-
-    return (
-        <div>
-            {data && <ResultCards result={data.message}/>}
-        </div>
-    )
+        if (param) {
+            fetchData(param);
+        } else {
+            console.log("no keyword");
+        }
+    }, [param]);
+    if (isLoading) {
+        return <div>Loading...</div>
+    } else {
+        return (
+            <div>
+                {data && <ResultCards result={data.message}/>}
+            </div>
+        )
+    }
 }
 
 export default ResultPage
