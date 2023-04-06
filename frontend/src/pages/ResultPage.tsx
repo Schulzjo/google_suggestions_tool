8cac13e1e6ab9ac1dbd617931967310f181dbb72
schulzjo <schulzeeverding.johannes@gmail.com>
2023-04-06 10:46:18 +0200

    add error dialog

--- frontend/src/pages/ResultPage.tsx
+++ frontend/src/pages/ResultPage.tsx
@@ -1,7 +1,7 @@
 import {useParams} from "react-router-dom"
 import ResultCards from "../components/ResultCards";
 import React, {useEffect, useState} from "react";
-import {CircularProgress} from "@mui/material";
+import {Alert, CircularProgress} from "@mui/material";
 
 interface ApiResponse {
     message: { [key: string]: string[] };
@@ -11,6 +11,7 @@ const ResultPage = () => {
     const {param} = useParams();
     const [data, setData] = useState<ApiResponse>();
     let [isLoading, setIsLoading] = useState<boolean>(true)
+    let [error, setError] = useState<string>("")
 
     function fetchData(keyword: string) {
         setIsLoading(true)
@@ -18,6 +19,12 @@ const ResultPage = () => {
             .then(response => response.json())
             .then(d => setData({...d}))
             .then(() => setIsLoading(false))
+            .catch((error) => {
+                    console.log(error)
+                    setError(error.message)
+                    setIsLoading(false)
+                }
+            )
     }
 
     useEffect(() => {
@@ -25,6 +32,7 @@ const ResultPage = () => {
             fetchData(param);
         } else {
             console.log("no keyword");
+            setError("No keyword found")
         }
     }, [param]);
 
@@ -33,7 +41,13 @@ const ResultPage = () => {
             <div className={"LoadingIndicator"}>
                 <CircularProgress size={60}/>
             </div>
-                )
+        )
+    } else if (error) {
+        return (
+            <div>
+                <Alert severity="error">{error}</Alert>
+            </div>
+        )
     } else {
         return (
             <div>
